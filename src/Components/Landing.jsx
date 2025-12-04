3c7cf2e6b2a3813858b81d56d569577e58b00957
Amr-7 <amrf258@gmail.com>
2025-12-04 20:38:16 +0800

    updates fix 2

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,15 +1,6 @@
-import pawsIntro from '../assets/meow.mp3'
-import landingImg from "../assets/paws-landing.png";
-import portraitImg from "../assets/paws-portrait.png";
 import './Landing.css'
 
 function Landing() {
-  useEffect(() => {
-    // Proper preloading using Image() with Vite-imported paths
-    new Image().src = landingImg;
-    new Image().src = portraitImg;
-  }, []);
-
   return (
     <div className="paws-landing">
       <p>A cat for everyone.</p>
