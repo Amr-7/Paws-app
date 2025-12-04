327b8347ed8c25f5b94a354fa37f8f460e63d6d8
Amr-7 <amrf258@gmail.com>
2025-12-04 20:17:03 +0800

    fixing

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -2,15 +2,6 @@ import pawsIntro from '../assets/meow.mp3'
 import './Landing.css'
 
 function Landing() {
-
-    useEffect(() => {
-    // Preload both landscape + portrait images
-    const images = [
-      new Image().src = '/Paws-app/assets/paws-landing.png',
-      new Image().src = '/Paws-app/assets/paws-portrait.png'
-    ];
-  }, []);
-  
   return (
     <div className="paws-landing">
       <p>A cat for everyone.</p>
