a35c6e6b00968970afdf8f1634a1951cb598bbfb
Amr-7 <amrf258@gmail.com>
2025-12-04 19:59:30 +0800

    preload landing images

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -2,10 +2,17 @@ import pawsIntro from '../assets/meow.mp3'
 import './Landing.css'
 
 function Landing() {
+
+    useEffect(() => {
+    // Preload both landscape + portrait images
+    const images = [
+      new Image().src = '/Paws-app/assets/paws-landing.png',
+      new Image().src = '/Paws-app/assets/paws-portrait.png'
+    ];
+  }, []);
+  
   return (
     <div className="paws-landing">
-        
-        
       <p>A cat for everyone.</p>
     </div>
   )
